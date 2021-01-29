66eb488b922ba21cf19bd1affce5df4f55f0c813
Hri7566 <hri7566@gmail.com>
2021-01-29 00:05:28 -0500

    karl came back

--- commands/rank.js
+++ commands/rank.js
@@ -1,5 +1,14 @@
 const Command = require(__approot+'/lib/Command');
+const Database = require(__approot+'/lib/Database');
 
 module.exports = new Command('rank', `Usage: PREFIXrank`, 0, (msg, bot) => {
-    return `${msg.p.name}, your rank is: ${msg.p.rank.name} | ID: ${msg.p.rank._id} | Room only: ${msg.p.rank.roomonly}`;
+    const user = bot.mainframe.findUser(msg.p._id);
+    if (typeof(user) == 'undefined') return;
+    if (typeof(user.rank) == 'undefined') return;
+    if (typeof(user.flags) == 'undefined') user.flags = {watchlist: false, god: false};
+    if (typeof(user.flags.god) == 'undefined') user.flags.god = false;
+
+    Database.save();
+
+    return `${msg.p.name}, your rank is: ${user.flags.god ? 'God' : msg.p.rank.name} | ID: ${msg.p.rank._id} | Room only: ${msg.p.rank.roomonly}`;
 }, 0, false);
